0cf86f481b91394e73e038dc5d12cf94836e3636
Nuttawut5z <65160165@go.buu.ac.th>
2024-12-12 14:02:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ from contextlib import asynccontextmanager
 app = FastAPI()
 
 # ข้อมูล token และ channel secret สำหรับ LINE
-ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN", "O0Vi8xE7Wh3A6BahSUC6O0VKR7RxR0p27jHBl1h39OdH9/d3cEtmrS4QT91BUEDmmrRqLrUiKLVxlJcggXWQ/MwNBJttPBjKEw8Oifg9O06on+Ab3UzbvQ7E8W56z5GeOIHvROzUsRVagavLPiTIbwdB04t89/1O/w1cDnyilFU=")
-CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET", "701c99f9fa1b4d0261e6f4dedcce76c8")
+ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN", "ITbsYQrp+uscj/p2teKTWgqjY0GTFdkLn+sfMysCBlbfofDLyjiQY2tppPfPB3pjR1CooIHaOUO/mcRgitnCtHACE66ihLa0nVbiyNLwO4ENDcWiIvaQgSozkr+8v50e8lyi7wk3fWE4a2z4AlIL9QdB04t89/1O/w1cDnyilFU=")
+CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET", "d22eab1091361f1b8498078ccb6a8ee9")
 
 # ข้อมูล Gemini api key
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyBrn9N8g0RnZrYhW-vFe3Tb2ytibKcsU3E")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyD02oaKCq21_Q-61xhTOtDpm_YeW2uQTJI")
 
 # การเชื่อมต่อ และตั้งค่าข้อมูลเพื่อเรียกใช้งาน LINE Messaging API
 configuration = Configuration(access_token=ACCESS_TOKEN)
@@ -240,10 +240,10 @@ gemini = GeminiRAGSystem(
 async def lifespan(app: FastAPI):
     # ข้อมูลตัวอย่างที่ใช้สำหรับ Gemini
     sample_documents = [
-        "จิรภัทร ทำดี คือ ชายหนุ่มที่มีความสามารถในการเขียนโปรแกรมที่มาจากบริษัท ClickNext ที่เป็นบริษัทด้านการพัฒนาโปรแกรมที่มีชื่อเสียง",
-        "จิรภัทร ทำดี มีชื่อเล่นว่า ปาล์ม เกิดวันที่ 25 มกราคม 2555 ที่จังหวัดสระบุรี ศึกษาจบจากมหาวิทยาลัยบูรพา สาขาวิศวกรรมคอมพิวเตอร์",
+        "ณัฐวุฒิ มัติโก คือ ชายหนุ่มที่มีความสามารถในการเขียนโปรแกรมที่มาจากมหาวิทยาลัยบูรพา คณะวิทยาการสารสนเทศ",
+        "ณัฐวุฒิ มัติโก มีชื่อเล่นว่า นัด เกิดวันที่ 19 มีนาคม 2546 ที่จังหวัดชลบุรี กำลังศึกษามหาวิทยาลัยบูรพา สาขาวิทยาการคอม",
         "งาน BUU-Bootcamp-2024 จัดที่มหาวิทยาลัยบูรพา ในวันที่ 25 มกราคม ปีค.ศ.2024 โดยมีการจัดกิจกรรมต่าง ๆ ที่เกี่ยวข้องกับการพัฒนาซอฟต์แวร์ เวลา 9:00 น. - 16:00 น.",
-        "มหาวิทยาลัยบูรพา สาขาวิชาAI ปีการศึกษา 2565 มีนักศึกษาจำนวน 100 คน มีอาจารย์ที่ปรึกษา 10 คน"
+        "มหาวิทยาลัยบูรพา สาขาวิชาcs ปีการศึกษา 2565 มีนักศึกษาจำนวน 110 คน มีอาจารย์ที่ปรึกษา 1 คน"
     ]
     
     # เพิ่มข้อมูลตัวอย่างลงใน Gemini
